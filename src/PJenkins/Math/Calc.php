4e73904b0443ad66c07062850f5c32331c8f66ea
Igor C. de Paula <principe.borodin@gmail.com>
2016-01-07 18:08:32 -0200

    Add tests files

---
+++ src/PJenkins/Math/Calc.php
@@ -0,0 +1,17 @@
+<?php
+Namespace PJenkins\Math;
+
+/**
+ * Class for Calcs
+ */
+class Calc
+{
+/**
+ * Sum 2 integers
+ * @param Integer $a An integer
+ * @param Integer $b An integer
+ */
+public function sum($a, $b){
+return $a + $b;
+}
+}
---
+++ tests/ProjetoTest/CalcTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ProjetoTest;
+
+require "../../vendor/autoload.php";
+use PJenkins\Math\Calc;
+
+class CalcTest extends Framework\TestCase
+{
+private $Calc;
+  protected function setUp()
+    {
+        parent::setUp();        
+        $this->Calc = new Calc();
+    }
+ protected function tearDown()
+    {
+        $this->Calc = null;        
+        parent::tearDown();
+    }
+ /**
+     * @covers PJenkins\Math\Calc::sum
+     */
+    public function testSum(){
+       
+       $this->assertEquals(5, $this->Calc->sum(2, 3));
+    }
+}
